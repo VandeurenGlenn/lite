52e74d7741476d5ded0c3885fb1366f369b95516
VandeurenGlenn <vandeurenglenn@gmail.com>
2024-03-10 13:48:55 +0100

    property: fix properties not set by attributes

--- src/decorators/property.ts
+++ src/decorators/property.ts
@@ -110,6 +110,8 @@ export const property = (options?: PropertyOptions) => {
           return set.call(this, value)
         },
         init(value: any): any {
+          if (value && this.hasAttribute(attributeName))
+            value = isBoolean ? this.hasAttribute(attributeName) : this.getAttribute(attributeName)
           if (value !== undefined) set.call(this, value)
           if (consumer && globalThis.pubsub.subscribers?.[name]?.value)
             set.call(this, globalThis.pubsub.subscribers[name].value)
