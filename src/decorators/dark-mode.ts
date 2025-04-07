0527c2dcd664d949512cda96db14cae52e7ed455
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2025-04-07 20:18:51 +0200

    improve. darkMode

--- src/decorators/dark-mode.ts
+++ src/decorators/dark-mode.ts
@@ -6,16 +6,19 @@ declare global {
   var pubsub: LittlePubSub
 }
 
-export function darkMode(provides?: boolean) {
+export function darkMode(provides?: boolean | string) {
   return (klass: Function, { addInitializer }) => {
     addInitializer(function () {
+      let propertyKey = 'darkMode'
+      if (typeof provides === 'string') propertyKey = provides
+
       const dark = window.matchMedia('(prefers-color-scheme: dark)')
 
       const changeMode = ({ matches }) => {
-        if (matches) this.darkMode = true
-        else this.darkMode = false
+        if (matches) this[propertyKey] = true
+        else this[propertyKey] = false
         this.requestRender()
-        if (provides) pubsub.publish('darkMode', this.darkMode)
+        if (provides) pubsub.publish(propertyKey, this.darkMode)
       }
 
       dark.addEventListener('change', changeMode)
